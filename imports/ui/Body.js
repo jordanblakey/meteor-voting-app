00459eb74554755c28df39db8b86652110a9eb36
Jordan Blakey <jordan@jordanblakey.com>
2017-01-18 05:14:39 -0600

    Quick commit.

--- imports/ui/Body.js
+++ imports/ui/Body.js
@@ -1,2 +1,19 @@
+import {Template} from 'meteor/templating';
+
+import {Items} from '../api/items.js'
+
 import './Body.html';
-import './Test.html';
+
+Template.body.helpers({
+    items() {
+        return Items.find({
+
+        });
+    }
+});
+
+Template.body.events({
+    'click .test'(event) {
+        console.log('hello');
+    }
+});
